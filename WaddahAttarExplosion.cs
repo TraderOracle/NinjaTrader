f4652c611665da9dbdeb00af3bcb9f622e7770c2
Trader Oracle <davesmith.artistic@gmail.com>
2024-03-12 14:53:11 -0500

    new

--- WaddahAttarExplosion.cs
+++ WaddahAttarExplosion.cs
@@ -19,6 +19,7 @@ using NinjaTrader.Data;
 using NinjaTrader.NinjaScript;
 using NinjaTrader.Core.FloatingPoint;
 using NinjaTrader.NinjaScript.DrawingTools;
+using System.Windows.Forms;
 #endregion
 
 //This namespace holds Indicators in this folder and is required. Do not change it. 
@@ -32,8 +33,9 @@ namespace NinjaTrader.NinjaScript.Indicators {
         protected int LastTime4 = 1;
         protected int Status = 0, PrevStatus = -1;
         protected double bask, bbid;
+        protected bool bIsGreen;
 
-        private string sound = NinjaTrader.Core.Globals.InstallDir + @"\sounds\Alert1.wav";
+        private string sound = NinjaTrader.Core.Globals.InstallDir + @"\sounds\waddah.wav";
 
         protected override void OnStateChange() {
             if (State == State.SetDefaults) {
@@ -50,7 +52,7 @@ namespace NinjaTrader.NinjaScript.Indicators {
                 //Disable this property if your indicator requires custom values that cumulate with each new market data event. 
                 //See Help Guide for additional information.
                 IsSuspendedWhileInactive = true;
-                Sensetive = 150;
+                Sensitive = 150;
                 DeadZonePip = 30;
                 ExplosionPower = 15;
                 TrendPower = 15;
@@ -68,73 +70,67 @@ namespace NinjaTrader.NinjaScript.Indicators {
             //Print(State);
         }
 
-        protected override void OnBarUpdate() {
-            if (CurrentBars[0] < 3) return;
+        protected override void OnBarUpdate()
+        {
+            if (CurrentBars[0] < 3) 
+                return;
+
             double Trend1, Trend2, Explo1, Explo2, Dead;
             double pwrt, pwre;
-            Trend1 = (MACD(20, 40, 9)[0] - MACD(20, 40, 9)[1]) * Sensetive;
-            Trend2 = (MACD(20, 40, 9)[2] - MACD(20, 40, 9)[3]) * Sensetive;
+            Trend1 = (MACD(20, 40, 9)[0] - MACD(20, 40, 9)[1]) * Sensitive;
+            Trend2 = (MACD(20, 40, 9)[2] - MACD(20, 40, 9)[3]) * Sensitive;
             Explo1 = Bollinger(2, 20).Upper[0] - Bollinger(2, 20).Lower[0];
             Explo2 = Bollinger(2, 20).Upper[1] - Bollinger(2, 20).Lower[1];
             Dead = TickSize * DeadZonePip;
+
+            SolidColorBrush br = Brushes.Green;
+            if (Trend1 < 0)
+                br = Brushes.Firebrick;
+
+            if (!bIsGreen && Trend1 > 0 && Trend1 > Explo1 && Trend1 > Trend2 && br == Brushes.Green && PlotBrushes[0][1] != Brushes.Lime)
+            {
+                br = Brushes.Lime;
+                PlotBrushes[0][0] = br;
+                bIsGreen = true;
+            }
+            if (bIsGreen && Trend1 < 0 && Math.Abs(Trend1) > Explo1 && Math.Abs(Trend1) > Math.Abs(Trend2) && br == Brushes.Firebrick && PlotBrushes[1][1] != Brushes.Red)
+            {
+                br = Brushes.Red;
+                PlotBrushes[1][0] = br;
+                bIsGreen = false;
+            }
+
             if (Trend1 >= 0) Values[0][0] = Trend1;
             if (Trend1 < 0) Values[1][0] = (-1 * Trend1);
             Values[2][0] = Explo1;
-            if (Trend1 > 0 && Trend1 > Explo1 && Trend1 > Dead && Explo1 > Dead && Explo1 > Explo2 && Trend1 > Trend2 &&
-                LastTime1 < AlertCount && AlertLong == true && GetCurrentAsk() != bask) {
+
+            if (Trend1 > 0 && Trend1 > Explo1 && Trend1 > Trend2 && LastTime1 < AlertCount && AlertLong == true)
+            {
                 pwrt = 100 * (Trend1 - Trend2) / Trend1;
                 pwre = 100 * (Explo1 - Explo2) / Explo1;
                 bask = GetCurrentAsk();
-                if (pwre >= ExplosionPower && pwrt >= TrendPower) {
-                    string message = String.Format("{0}- {1}, - BUY ({2:F5}) Trend PWR {3:F0} - Exp PWR {4:F0}", LastTime1, Instrument.FullName, bask, pwrt, pwre);
-                    if (AlertWindow == true) {
-                        Alert("Alert", Priority.Medium, message, sound, 10, Brushes.Black, Brushes.BlanchedAlmond);
-                    }
-                    //Print(message);
-                    LastTime1++;
-                }
+                string message = String.Format("{0}- {1}, - BUY ({2:F5}) Trend PWR {3:F0} - Exp PWR {4:F0}", LastTime1, Instrument.FullName, bask, pwrt, pwre);
+                if (AlertWindow == true)
+                    Alert("Alert", Priority.Medium, message, sound, 10, Brushes.Black, Brushes.BlanchedAlmond);
+                Print(message);
+                LastTime1++;
                 Status = 1;
             }
-            if (Trend1 < 0 && Math.Abs(Trend1) > Explo1 && Math.Abs(Trend1) > Dead && Explo1 > Dead && Explo1 > Explo2 &&
-                Math.Abs(Trend1) > Math.Abs(Trend2) && LastTime2 < AlertCount && AlertShort == true && GetCurrentBid() != bbid) {
+            if (Trend1 < 0 && Math.Abs(Trend1) > Explo1 && Math.Abs(Trend1) > Math.Abs(Trend2) && LastTime2 < AlertCount && AlertShort == true)
+            {
                 pwrt = 100 * (Math.Abs(Trend1) - Math.Abs(Trend2)) / Math.Abs(Trend1);
                 pwre = 100 * (Explo1 - Explo2) / Explo1;
                 bbid = GetCurrentBid();
-                if (pwre >= ExplosionPower && pwrt >= TrendPower) {
-                    string message = String.Format("{0}- {1}, - SELL ({2:F5}) Trend PWR {3:F0} - Exp PWR {4:F0}", LastTime2, Instrument.FullName, bbid, pwrt, pwre);
-                    if (AlertWindow == true) {
-                        Alert("Alert", Priority.Medium, message, sound, 10, Brushes.Black, Brushes.BlanchedAlmond);
-                    }
-                    //Print(message);
-                    LastTime2++;
-                }
+                string message = String.Format("{0}- {1}, - SELL ({2:F5}) Trend PWR {3:F0} - Exp PWR {4:F0}", LastTime2, Instrument.FullName, bbid, pwrt, pwre);
+                if (AlertWindow == true)
+                    Alert("Alert", Priority.Medium, message, sound, 10, Brushes.Black, Brushes.BlanchedAlmond);
+                Print(message);
+                LastTime2++;
                 Status = 2;
             }
-            if (Trend1 > 0 && Trend1 < Explo1 && Trend1 < Trend2 && Trend2 > Explo2 && Trend1 > Dead && Explo1 > Dead &&
-                LastTime3 <= AlertCount && AlertExitLong == true && GetCurrentBid() != bbid) {
-                bbid = GetCurrentBid();
-                string message = String.Format("{0}- {1} - Exit BUY  {2:F5}", LastTime3, Instrument.FullName, bbid);
-                if (AlertWindow == true) {
-                    Alert("Alert", Priority.Medium, message, sound, 10, Brushes.Black, Brushes.BlanchedAlmond);
-                }
-                //Print(message);
-                Status = 3;
-                LastTime3++;
-            }
-            if (Trend1 < 0 && Math.Abs(Trend1) < Explo1 && Math.Abs(Trend1) < Math.Abs(Trend2) &&
-                Math.Abs(Trend2) > Explo2 && Trend1 > Dead && Explo1 > Dead && LastTime4 <= AlertCount &&
-                AlertExitShort == true && GetCurrentAsk() != bask) {
-                bask = GetCurrentAsk();
-                string message = String.Format("{0}- {1} - Exit SELL  {2:F5}", LastTime4, Instrument.FullName, bask);
-                if (AlertWindow == true) {
-                    Alert("Alert", Priority.Medium, message, sound, 10, Brushes.Black, Brushes.BlanchedAlmond);
-                }
-                //Print(message);
-                Status = 4;
-                LastTime4++;
-            }
             PrevStatus = Status;
-            if (Status != PrevStatus) {
+            if (Status != PrevStatus)
+            {
                 LastTime1 = 1;
                 LastTime2 = 1;
                 LastTime3 = 1;
@@ -145,8 +141,8 @@ namespace NinjaTrader.NinjaScript.Indicators {
         #region Properties
         [NinjaScriptProperty]
         [Range(1, int.MaxValue)]
-        [Display(Name = "Sensetive", Description = "Sensetive Value", Order = 1, GroupName = "Parameters")]
-        public int Sensetive { get; set; }
+        [Display(Name = "Sensitive", Description = "Sensitive Value", Order = 1, GroupName = "Parameters")]
+        public int Sensitive { get; set; }
 
         [NinjaScriptProperty]
         [Range(1, int.MaxValue)]
@@ -223,18 +219,18 @@ namespace NinjaTrader.NinjaScript.Indicators
 	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
 	{
 		private WaddahAttarExplosion[] cacheWaddahAttarExplosion;
-		public WaddahAttarExplosion WaddahAttarExplosion(int sensetive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
+		public WaddahAttarExplosion WaddahAttarExplosion(int sensitive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
 		{
-			return WaddahAttarExplosion(Input, sensetive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
+			return WaddahAttarExplosion(Input, sensitive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
 		}
 
-		public WaddahAttarExplosion WaddahAttarExplosion(ISeries<double> input, int sensetive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
+		public WaddahAttarExplosion WaddahAttarExplosion(ISeries<double> input, int sensitive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
 		{
 			if (cacheWaddahAttarExplosion != null)
 				for (int idx = 0; idx < cacheWaddahAttarExplosion.Length; idx++)
-					if (cacheWaddahAttarExplosion[idx] != null && cacheWaddahAttarExplosion[idx].Sensetive == sensetive && cacheWaddahAttarExplosion[idx].DeadZonePip == deadZonePip && cacheWaddahAttarExplosion[idx].ExplosionPower == explosionPower && cacheWaddahAttarExplosion[idx].TrendPower == trendPower && cacheWaddahAttarExplosion[idx].AlertWindow == alertWindow && cacheWaddahAttarExplosion[idx].AlertCount == alertCount && cacheWaddahAttarExplosion[idx].AlertLong == alertLong && cacheWaddahAttarExplosion[idx].AlertShort == alertShort && cacheWaddahAttarExplosion[idx].AlertExitLong == alertExitLong && cacheWaddahAttarExplosion[idx].AlertExitShort == alertExitShort && cacheWaddahAttarExplosion[idx].EqualsInput(input))
+					if (cacheWaddahAttarExplosion[idx] != null && cacheWaddahAttarExplosion[idx].Sensitive == sensitive && cacheWaddahAttarExplosion[idx].DeadZonePip == deadZonePip && cacheWaddahAttarExplosion[idx].ExplosionPower == explosionPower && cacheWaddahAttarExplosion[idx].TrendPower == trendPower && cacheWaddahAttarExplosion[idx].AlertWindow == alertWindow && cacheWaddahAttarExplosion[idx].AlertCount == alertCount && cacheWaddahAttarExplosion[idx].AlertLong == alertLong && cacheWaddahAttarExplosion[idx].AlertShort == alertShort && cacheWaddahAttarExplosion[idx].AlertExitLong == alertExitLong && cacheWaddahAttarExplosion[idx].AlertExitShort == alertExitShort && cacheWaddahAttarExplosion[idx].EqualsInput(input))
 						return cacheWaddahAttarExplosion[idx];
-			return CacheIndicator<WaddahAttarExplosion>(new WaddahAttarExplosion(){ Sensetive = sensetive, DeadZonePip = deadZonePip, ExplosionPower = explosionPower, TrendPower = trendPower, AlertWindow = alertWindow, AlertCount = alertCount, AlertLong = alertLong, AlertShort = alertShort, AlertExitLong = alertExitLong, AlertExitShort = alertExitShort }, input, ref cacheWaddahAttarExplosion);
+			return CacheIndicator<WaddahAttarExplosion>(new WaddahAttarExplosion(){ Sensitive = sensitive, DeadZonePip = deadZonePip, ExplosionPower = explosionPower, TrendPower = trendPower, AlertWindow = alertWindow, AlertCount = alertCount, AlertLong = alertLong, AlertShort = alertShort, AlertExitLong = alertExitLong, AlertExitShort = alertExitShort }, input, ref cacheWaddahAttarExplosion);
 		}
 	}
 }
@@ -243,14 +239,14 @@ namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
 {
 	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
 	{
-		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(int sensetive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
+		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(int sensitive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
 		{
-			return indicator.WaddahAttarExplosion(Input, sensetive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
+			return indicator.WaddahAttarExplosion(Input, sensitive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
 		}
 
-		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(ISeries<double> input , int sensetive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
+		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(ISeries<double> input , int sensitive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
 		{
-			return indicator.WaddahAttarExplosion(input, sensetive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
+			return indicator.WaddahAttarExplosion(input, sensitive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
 		}
 	}
 }
@@ -259,14 +255,14 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
 	{
-		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(int sensetive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
+		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(int sensitive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
 		{
-			return indicator.WaddahAttarExplosion(Input, sensetive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
+			return indicator.WaddahAttarExplosion(Input, sensitive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
 		}
 
-		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(ISeries<double> input , int sensetive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
+		public Indicators.WaddahAttarExplosion WaddahAttarExplosion(ISeries<double> input , int sensitive, int deadZonePip, int explosionPower, int trendPower, bool alertWindow, int alertCount, bool alertLong, bool alertShort, bool alertExitLong, bool alertExitShort)
 		{
-			return indicator.WaddahAttarExplosion(input, sensetive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
+			return indicator.WaddahAttarExplosion(input, sensitive, deadZonePip, explosionPower, trendPower, alertWindow, alertCount, alertLong, alertShort, alertExitLong, alertExitShort);
 		}
 	}
 }
