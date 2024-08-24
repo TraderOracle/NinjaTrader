9a2285e85ad8cdfe9818e696aeb66c732caf62bc
Trader Oracle <157963314+TraderOracle@users.noreply.github.com>
2024-08-24 03:36:01 -0500

    Update ManciniNinja.cs

--- ManciniNinja.cs
+++ ManciniNinja.cs
@@ -1,47 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
 using NinjaTrader.Gui.Tools;
-using NinjaTrader.NinjaScript;
 using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.DrawingTools;
+using NinjaTrader.NinjaScript.SuperDomColumns;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Markup;
+using Infragistics.Windows.DataPresenter;
 
 namespace NinjaTrader.NinjaScript.Indicators
 {
     public class ConfigurableHorizontalLine : Indicator
     {
-        private String Support;
-        private String Resist;
+//        private String Support;
+//        private String Resist;
 
-        private Brush colSupport;
-        private Brush colResist;
-        private Brush colMajorSupport;
-        private Brush colMajorResist;
-
-        [NinjaScriptProperty]
-        [Display(Name = "Mancini Supports", Description = "5343,5233,6046, etc", Order = 1, GroupName = "Lines")]
-        public String Support { get; set; }
-
-        [NinjaScriptProperty]
-        [Display(Name = "Mancini Resistances", Description = "5343,5233,6046, etc", Order = 2, GroupName = "Lines")]
-        public String Resist { get; set; }
-
-        [NinjaScriptProperty]
-        [XmlIgnore]
-        [Display(Name = "Support Color", Order = 3, GroupName = "Colors")]
-        public Brush colSupport { get; set; }
-
-        [NinjaScriptProperty]
-        [XmlIgnore]
-        [Display(Name = "Resistance Color", Order = 4, GroupName = "Colors")]
-        public Brush colResist { get; set; }
-
-        [NinjaScriptProperty]
-        [XmlIgnore]
-        [Display(Name = "Major Support Color", Order = 5, GroupName = "Colors")]
-        public Brush colMajorSupport { get; set; }
-
-        [NinjaScriptProperty]
-        [XmlIgnore]
-        [Display(Name = "Major Resistance Color", Order = 6, GroupName = "Colors")]
-        public Brush colMajorResist { get; set; }
+//        private Brush colSupport;
+//        private Brush colResist;
+//        private Brush colMajorSupport;
+//        private Brush colMajorResist;
 
         //[Browsable(false)]
         //public string LineColorSerializable
@@ -77,25 +76,27 @@ namespace NinjaTrader.NinjaScript.Indicators
             }
         }
 
-        protected override void OnBarUpdate()
-        {
-            int idx = 1;
+		private void DrawLines()
+		{
+			int idx = 1;
             String[] sr = Support.Split(',');
-            foreach (String s in sr)
+            foreach (string s in sr)
             {
+				string sa = s.Trim();
+				
                 if (s.Contains("-"))
                 {
                     if (s.Contains("Major"))
                     {
-                        s = s.Replace(" (Major)", "").Trim();
-                        s = s.SubString(0, 4);
-                        double dPrice = Convert.Double(s.Trim());
+                        sa = s.Replace(" (Major)", "").Trim();
+                        sa = sa.Substring(0, 4);
+                        double dPrice = Convert.ToDouble(sa.Trim());
                         Draw.HorizontalLine(this, "ho" + idx, dPrice, colMajorSupport, DashStyleHelper.Solid, 1);
                     }
                     else
                     {
-                        s = s.SubString(0, 4);
-                        double dPrice = Convert.Double(s.Trim());
+                        sa = s.Substring(0, 4);
+                        double dPrice = Convert.ToDouble(sa.Trim());
                         Draw.HorizontalLine(this, "ho" + idx, dPrice, colSupport, DashStyleHelper.Solid, 1);
                     }
                 }
@@ -103,18 +104,109 @@ namespace NinjaTrader.NinjaScript.Indicators
                 {
                     if (s.Contains("Major"))
                     {
-                        s = s.Replace(" (Major)", "").Trim();
-                        double dPrice = Convert.Double(s.Trim());
+                        sa = s.Replace(" (Major)", "").Trim();
+                        double dPrice = Convert.ToDouble(sa.Trim());
                         Draw.HorizontalLine(this, "ho" + idx, dPrice, colMajorSupport, DashStyleHelper.Solid, 1);
                     }
                     else
                     {
-                        double dPrice = Convert.Double(s.Trim());
+                        sa = s.Substring(0, 4);
+                        double dPrice = Convert.ToDouble(sa.Trim());
                         Draw.HorizontalLine(this, "ho" + idx, dPrice, colSupport, DashStyleHelper.Solid, 1);
                     }
                 }
                 idx++;
             }
+		}
+		
+        protected override void OnBarUpdate()
+        {
+			DrawLines();
         }
+		
+		#region Properties
+		
+		[NinjaScriptProperty]
+        [Display(Name = "Mancini Supports", Description = "5343,5233,6046, etc", Order = 1, GroupName = "Lines")]
+        public string Support { get; set; }
+
+        [NinjaScriptProperty]
+        [Display(Name = "Mancini Resistances", Description = "5343,5233,6046, etc", Order = 2, GroupName = "Lines")]
+        public string Resist { get; set; }
+
+		[XmlIgnore]
+        [Display(Name = "Support Color", Order = 3, GroupName = "Colors")]
+        public Brush colSupport { get; set; }
+
+		[XmlIgnore]
+        [Display(Name = "Resistance Color", Order = 4, GroupName = "Colors")]
+        public Brush colResist { get; set; }
+
+		[XmlIgnore]
+        [Display(Name = "Major Support Color", Order = 5, GroupName = "Colors")]
+        public Brush colMajorSupport { get; set; }
+
+		[XmlIgnore]
+        [Display(Name = "Major Resistance Color", Order = 6, GroupName = "Colors")]
+        public Brush colMajorResist { get; set; }
+
+		#endregion
     }
 }
+
+#region NinjaScript generated code. Neither change nor remove.
+
+namespace NinjaTrader.NinjaScript.Indicators
+{
+	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
+	{
+		private ConfigurableHorizontalLine[] cacheConfigurableHorizontalLine;
+		public ConfigurableHorizontalLine ConfigurableHorizontalLine(string support, string resist)
+		{
+			return ConfigurableHorizontalLine(Input, support, resist);
+		}
+
+		public ConfigurableHorizontalLine ConfigurableHorizontalLine(ISeries<double> input, string support, string resist)
+		{
+			if (cacheConfigurableHorizontalLine != null)
+				for (int idx = 0; idx < cacheConfigurableHorizontalLine.Length; idx++)
+					if (cacheConfigurableHorizontalLine[idx] != null && cacheConfigurableHorizontalLine[idx].Support == support && cacheConfigurableHorizontalLine[idx].Resist == resist && cacheConfigurableHorizontalLine[idx].EqualsInput(input))
+						return cacheConfigurableHorizontalLine[idx];
+			return CacheIndicator<ConfigurableHorizontalLine>(new ConfigurableHorizontalLine(){ Support = support, Resist = resist }, input, ref cacheConfigurableHorizontalLine);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
+{
+	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
+	{
+		public Indicators.ConfigurableHorizontalLine ConfigurableHorizontalLine(string support, string resist)
+		{
+			return indicator.ConfigurableHorizontalLine(Input, support, resist);
+		}
+
+		public Indicators.ConfigurableHorizontalLine ConfigurableHorizontalLine(ISeries<double> input , string support, string resist)
+		{
+			return indicator.ConfigurableHorizontalLine(input, support, resist);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
+	{
+		public Indicators.ConfigurableHorizontalLine ConfigurableHorizontalLine(string support, string resist)
+		{
+			return indicator.ConfigurableHorizontalLine(Input, support, resist);
+		}
+
+		public Indicators.ConfigurableHorizontalLine ConfigurableHorizontalLine(ISeries<double> input , string support, string resist)
+		{
+			return indicator.ConfigurableHorizontalLine(input, support, resist);
+		}
+	}
+}
+
+#endregion
